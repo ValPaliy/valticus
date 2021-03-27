3da34c4024d2752d603f2c930502ed2ab9e2c0aa
Val Paliy <valpaliy@gmail.com>
2021-03-27 13:52:29 +0200

    Test post

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ using Statiq.Web;
 
 namespace valticus
 {
-  public class Program
-  {
-    public static async Task<int> Main(string[] args) =>
-      await Bootstrapper
-        .Factory
-        .CreateWeb(args)
-        .RunAsync();
-  }
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+          await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .RunAsync();
+    }
 }
